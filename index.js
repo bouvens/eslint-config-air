d8b02645f98b5d00769bf3b10ec13300b3b0552e
Alexander Demin <bouvens@gmail.com>
2021-09-10 17:36:51 +0600

    refactor: Extract non-React to air-base config

--- index.js
+++ index.js
@@ -1,27 +1,6 @@
 module.exports = {
-  parser: '@babel/eslint-parser',
-  env: {
-    browser: true,
-    es6: true,
-    jasmine: true,
-  },
-  extends: 'airbnb',
-  plugins: [
-    'import',
-    'react',
-  ],
+  extends: ['airbnb', 'air-base'],
   rules: {
-    'complexity': ['warn', 12],
-    'consistent-this': ['warn', 'that'],
-    'curly': ['warn', 'all'],
-    'linebreak-style': 'off',
-    'no-multiple-empty-lines': 'warn',
-    'no-plusplus': ['error', { 'allowForLoopAfterthoughts': true }],
-    'no-trailing-spaces': ['error', { skipBlankLines: true }],
-    'no-unexpected-multiline': 'error',
-    'object-curly-newline': ['off'],
-    'semi': ['warn', 'never'],
-    'valid-jsdoc': ['error'],
     'jsx-a11y/label-has-for': [
       'warn', {
         'components': ['Label'],
